7add91fd8e1c45a63103bd561e7b960b327ce207
Gh057Pr1nc3 <ghostprince.1648@gmail.com>
2020-05-19 21:52:26 +0700

    Update Get-DllInjection.ps1

--- Get-DllInjection.ps1
+++ Get-DllInjection.ps1
@@ -9,28 +9,24 @@ public static extern bool IsWow64Process(
     [Out, MarshalAs(UnmanagedType.Bool)] out bool wow64Process);
 '@ -Name NativeMethods -Namespace Kernel32
 
-#$AllInMemoryOrderModuleList = @()
+$AllInMemoryOrderModuleList = @()
 
-Get-Process | ForEach-Object {
+$DllModule = Get-Process | Where-Object {$_ -ne $null -and $_.Path.StartsWith("C:\Windows\") -and $_.Id -ne $PID} | ForEach-Object {
     Write-Host $_.Id 
     Write-Host $_.Name
     $Process = $_
     $is32Bit=[int]0 
     $a = [Kernel32.NativeMethods]::IsWow64Process($_.Handle, [ref]$is32Bit)
     if($is32Bit){
-        #&"$env:WINDIR\SysWow64\windowspowershell\v1.0\powershell.exe"
-        #Import-Module .\PowerShellArsenal\PowerShellArsenal.psd1 -Force
-        #$PEB = Get-PEB -Id $_.Id
         $InMemoryOrderModuleList = &"$env:windir\SysWow64\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -Command "(Get-PEB -Id $($_.Id)).InMemoryOrderModuleList | ConvertTo-CSV" | ConvertFrom-Csv
-        #&"$env:WINDIR\System32\windowspowershell\v1.0\powershell.exe"
 		
     }
     else {
         $InMemoryOrderModuleList = (Get-PEB -Id $_.Id).InMemoryOrderModuleList
     }
-#	$AllInMemoryOrderModuleList += $InMemoryOrderModuleList
+	$AllInMemoryOrderModuleList += $InMemoryOrderModuleList
     $InMemoryOrderModuleList | Where-Object {
-	$_.ProcessAttachCalled -eq $false -and $_.FullDllName -ne "C:\Windows\SYSTEM32\ntdll.dll" -and $_.FullDllName -ne $Process.MainModule.FileName
+	$_.ProcessAttachCalled -eq $false -and $_.BaseDllName -ne "ntdll.dll" -and $_.FullDllName -ne $Process.MainModule.FileName
 	} | Select-Object BaseAddress, SizeOfImage, FullDllName, BaseDllName | ForEach-Object{
 		$_ | Add-Member NoteProperty ProcessName $Process.ProcessName -Force
 		$_ | Add-Member NoteProperty ProcessID $Process.Id -Force
@@ -38,6 +34,5 @@ Get-Process | ForEach-Object {
 		$_
 	}
 }
-#$AllModule = ($AllInMemoryOrderModuleList | Select-Object BaseAddress, SizeOfImage, FullDllName, BaseDllName | Group-Object FullDllName | Where-Object { $_.Count -eq 1 }).Values
-
-#$DllModule | Where-Object { $_.FullDllName -in $AllModule}
+$AllModule = ($AllInMemoryOrderModuleList | Select-Object FullDllName | Group-Object FullDllName | Where-Object { $_.Count -eq 1 }).Values
+$DllModule | Where-Object { $_.FullDllName -in $AllModule}
